f367df087f8e7d91e4d573d4a11de63c23786d58
Bishoy Boktor <bishoy.boktorsd@mtsu.edu>
2019-01-25 22:35:32 -0600

    Add tgaimage support

---
+++ tgaimage.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <fstream>
+#include <time.h>
+#include <string.h>
+#include <math.h>
+#include "tgaimage.h"
+
+TGAImage::TGAImage() : data(NULL), width(0), height(0), bytespp(0) {
+}
+
+TGAImage::TGAImage(int w, int h, int bpp) : data(NULL), width(w), height(h), bytespp(bpp) {
+	unsigned long nbytes = width*height*bytespp;
+	data = new unsigned char[nbytes];
+	memset(data, 0, nbytes);
+}
+
+TGAImage::TGAImage(TGAImage &img) {
+	width = img.width;
+	height = img.height;
+	bytespp = img.bytespp;
+	unsigned long nbytes = width*height*bytespp;
